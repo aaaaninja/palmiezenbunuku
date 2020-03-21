af40fcadd81b963bd10f8f9b8bf5fc07c99c1750
aaaaninja <aaaaninja@aaaa.ninja>
2020-03-21 22:37:11 +0900

    get course name and URLs

--- jk.js
+++ jk.js
@@ -14,6 +14,11 @@ const puppeteer = require('puppeteer');
   await page.waitFor('a.sc-bdVaJa.gtVQmG')
   await page.waitFor(3000)
 
+  const name_and_urls = await page.$$eval('a.sc-bdVaJa.gtVQmG', course_list => {
+    return course_list.map(course => [course.querySelector('div.sc-bdVaJa.gRaSAC').textContent, course.href])
+  })
+  console.log(name_and_urls)
+
   debugger;
   await browser.close();
 })();
