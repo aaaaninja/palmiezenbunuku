6d323466a6bcc32ebf40f0c3de2a0565247cc224
aaaaninja <aaaaninja@aaaa.ninja>
2020-03-27 11:35:05 +0900

    sectionURLs.jsからとりあえず形を移植

--- lessonURLs.js
+++ lessonURLs.js
@@ -1,8 +1,9 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs').promises;
 
 (async () => {
   const browser = await puppeteer.launch(
-    { headless: false
+    { headless: true
     , devtools: true
     }
   )
@@ -17,17 +18,17 @@ const puppeteer = require('puppeteer');
   const course_name_and_urls = await page.$$eval('a.sc-bdVaJa.gtVQmG', course_list => {
     return course_list.map(course => [course.querySelector('div.sc-bdVaJa.gRaSAC').textContent, course.href])
   })
-  console.log(course_name_and_urls)
+  console.error(course_name_and_urls)
 
   for (const [name, url] of course_name_and_urls) {
-    console.log(`start --- ${name}`)
+    console.error(`start --- ${name}`)
 
     await page.goto(url, { waitUntil: 'domcontentloaded' })
     await page.waitFor('div.u-list.has-linked-children > li > a')
     const chapter_name_and_urls = await page.$$eval('div.u-list.has-linked-children > li > a', chapter_list => chapter_list.map(chapter => [chapter.querySelector('p').textContent, chapter.href]))
-    console.log(chapter_name_and_urls)
+    console.error(chapter_name_and_urls)
     for (const [chapter_name, chapter_url] of chapter_name_and_urls) {
-      console.log(`start --- --- ${chapter_name}`)
+      console.error(`start --- --- ${chapter_name}`)
 
       await page.goto(chapter_url, { waitUntil: 'domcontentloaded' })
       await page.waitFor('div[data-react-class="App.Chapter"]')
@@ -36,13 +37,28 @@ const puppeteer = require('puppeteer');
             .chapter
             .chapter
             .class_headers
-      console.log(section.sections, lesson.sections)
-      console.log(`end --- --- ${chapter_name}`)
+      section.sections.forEach(section_content => console.log(section_content.content_url))
+      const dir_name_content = `${name.replace(/[(\\|/|:|\\*|?|\"|<|>|\\\\|)]/g, '')}/${chapter_name.replace(/[(\\|/|:|\\*|?|\"|<|>|\\\\|)]/g, '')}`
+      console.error(dir_name_content)
+      await fs.mkdir(dir_name_content, { recursive: true })
+      await fs.writeFile(`${dir_name_content}/sections.json`, JSON.stringify(section))
+      for (const section_content of section.sections) {
+        await page.goto(section_content.content_url, { waitUntil: 'networkidle2' }) // こんな感じのurl => https://www.nnn.ed.nico/contents/links/90253?content_type=n-yobi or https://www.nnn.ed.nico/contents/guides/2158/content (こっちはid)
+        await page.waitFor(2000)
+        const content_height = await page.evaluate(() => document.querySelector('div.container').scrollHeight) // ほんとは `document.documentElement.offsetHeight` ってやりたいんだけど、nはなんかこれじゃ取れなかった (githubとかは可)
+        await page.pdf( // https://github.com/puppeteer/puppeteer/issues/475
+          { path: `${dir_name_content}/${section_content.title.replace(/[(\\|/|:|\\*|?|\"|<|>|\\\\|)]/g, '')}.pdf`
+          , printBackground: true
+          , margin: "none"
+          , height: `${content_height + 1}px`
+          }
+        )
+      }
+      console.error(`end --- --- ${chapter_name}`)
     }
 
-    console.log(`end --- ${name}`)
+    console.error(`end --- ${name}`)
   }
 
-  debugger;
   await browser.close();
 })();
