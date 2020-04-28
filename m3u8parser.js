a56a1be8030e51591991bb1f60da1a9efb6e778f
aaaaninja <aaaaninja@aaaa.ninja>
2020-04-29 00:04:21 +0900

    はい (master.m3u8でplaylistをひっぱってこれるように)

--- m3u8parser.js
+++ m3u8parser.js
@@ -1,24 +1,14 @@
 const m3u8Parser = require('m3u8-parser');
 const url = require('url');
+const axios = require('axios');
 
 const parser = new m3u8Parser.Parser();
 
-const manifest = `
-#EXTM3U
-#EXT-X-INDEPENDENT-SEGMENTS
-#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-medium",NAME="audio",AUTOSELECT=YES,DEFAULT=YES,CHANNELS="2",URI="../../audio/f7dd75fe/playlist.m3u8"
-
-#EXT-X-STREAM-INF:CLOSED-CAPTIONS=NONE,BANDWIDTH=6153099,AVERAGE-BANDWIDTH=3931000,RESOLUTION=1920x1080,FRAME-RATE=29.970,CODECS="avc1.640828,mp4a.40.2",AUDIO="audio-medium"
-../f7dd75fe/playlist.m3u8
-#EXT-X-STREAM-INF:CLOSED-CAPTIONS=NONE,BANDWIDTH=2693762,AVERAGE-BANDWIDTH=1472000,RESOLUTION=1280x720,FRAME-RATE=29.970,CODECS="avc1.640820,mp4a.40.2",AUDIO="audio-medium"
-../307e9c98/playlist.m3u8
-#EXT-X-STREAM-INF:CLOSED-CAPTIONS=NONE,BANDWIDTH=1681664,AVERAGE-BANDWIDTH=931000,RESOLUTION=960x540,FRAME-RATE=29.970,CODECS="avc1.64081F,mp4a.40.2",AUDIO="audio-medium"
-../3f066d0c/playlist.m3u8
-#EXT-X-STREAM-INF:CLOSED-CAPTIONS=NONE,BANDWIDTH=481123,AVERAGE-BANDWIDTH=319000,RESOLUTION=426x240,FRAME-RATE=29.970,CODECS="avc1.640815,mp4a.40.2",AUDIO="audio-medium"
-../d0e01c31/playlist.m3u8
-#EXT-X-STREAM-INF:CLOSED-CAPTIONS=NONE,BANDWIDTH=698532,AVERAGE-BANDWIDTH=506000,RESOLUTION=640x360,FRAME-RATE=29.970,CODECS="avc1.64081E,mp4a.40.2",AUDIO="audio-medium"
-../d899ec07/playlist.m3u8
-`.trim()
+(async () => {
+const manifest =
+  (await axios.get('https://skyfire.vimeocdn.com/1588087802-0x721ded24d5c3e8492f41845626dea70906739aed/d0dfbcc5-7389-4fc5-a981-10a7e94c7e0a/sep/video/2671bd23,2f72e2ca,529b19c5,dd3c23da,e7b67bcf/master.m3u8'))
+  .data
+  .trim()
 
 parser.push(manifest)
 parser.end()
@@ -32,6 +22,9 @@ const video_m3u8 = url.resolve(pathto, highest_resolution.uri)
 const audio_of_that = parsed_manifest.mediaGroups.AUDIO[highest_resolution.attributes.AUDIO]
 const audio_m3u8 = url.resolve(pathto, audio_of_that.audio.uri)
 
-debugger;
+console.log(video_m3u8)
+console.log(audio_m3u8)
 
+debugger;
 console.log('asdf')
+})()
