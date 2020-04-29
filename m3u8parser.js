a5e4eee0e613d3e09e808fa48376b2ba14a924ac
aaaaninja <aaaaninja@aaaa.ninja>
2020-04-29 17:09:02 +0900

    はい (ただの行入れ替え)

--- m3u8parser.js
+++ m3u8parser.js
@@ -16,8 +16,8 @@ const master_m3u8 = process.argv[2]
   const parsed_manifest = parser.manifest
 
   const highest_resolution = parsed_manifest.playlists.reduce((a,b)=> a.attributes.BANDWIDTH > b.attributes.BANDWIDTH ? a : b )
-  const video_m3u8 = url.resolve(master_m3u8, highest_resolution.uri)
   const audio_of_that = parsed_manifest.mediaGroups.AUDIO[highest_resolution.attributes.AUDIO]
+  const video_m3u8 = url.resolve(master_m3u8, highest_resolution.uri)
   const audio_m3u8 = url.resolve(master_m3u8, audio_of_that.audio.uri)
 
   console.log(video_m3u8)
