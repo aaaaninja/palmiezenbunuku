e5654212a7b42d0cbce33a537ac16ac2728cacf9
aaaaninja <aaaaninja@aaaa.ninja>
2020-05-01 17:44:09 +0900

    はい (とくに要らないやつら)

--- vi/ff.sh
+++
@@ -1,6 +0,0 @@
-ffmpeg -i 'https://skyfire.vimeocdn.com/1588253379-0xe7b136d79fb2e876f63885e81b80e85680af0b0a/247926400/sep/video/1259135044,1259135043/master.m3u8' \
-  -map p:1 \
-  -movflags faststart \
-  -acodec copy \
-  -bsf:a aac_adtstoasc \
-  -c copy jk.mp4
--- vi/ffm.sh
+++
@@ -1,6 +0,0 @@
-ffmpeg -i 'https://31vod-adaptive.akamaized.net/exp=1588067766~acl=%2Fd229e0bb-a182-47e9-af79-272875aee13e%2F%2A~hmac=ca92e68c6a3d0f44a2f52d61dc4d3813cbac401b88fe9ef4422ce3340aa603b2/d229e0bb-a182-47e9-af79-272875aee13e/sep/video/ee8d9152/playlist.m3u8' \
-  -map p:1 \
-  -movflags faststart \
-  -acodec copy \
-  -bsf:a aac_adtstoasc \
-  -c copy reccc.mp4
