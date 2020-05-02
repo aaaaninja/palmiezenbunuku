9ae7233ec02b203af014164315b6c1ea177d6e52
aaaaninja <aaaaninja@aaaa.ninja>
2020-05-03 01:07:44 +0900

    いらん

--- vi/yd.sh
+++
@@ -1,3 +0,0 @@
-youtube-dl -f bestvideo+audio-high-audio/audio-medium-audio \
-  'https://skyfire.vimeocdn.com/1588240239-0x2bd695d87ee95cbf5dffa03018ad3528d444da91/b9140323-9218-4baa-93ce-18cc831c8aa3/sep/video/adc34c8e,c39e66ef,0ee981a6,6b4c9000,6f10f7aa/master.m3u8'
-
